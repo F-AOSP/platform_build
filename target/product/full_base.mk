TeamLiteSpeed/android_build

Inherit vendor/aod if exists and only set backup true if exists
Change-Id: I5787e05d73d9e29be3bb0c80c58f19d389d94fb4

target/product/full_base.mk
@@ -61,3 +61,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
+
+# Call the vendor if it exists
+$(call inherit-product-if-exists, vendor/aod/vendor.mk)

F-AOSP/platform_build

target/product/full_base.mk
@@ -48,3 +48,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
+
+# Call the vendor if it exists
+$(call inherit-product-if-exists, vendor/aosp/common.mk)