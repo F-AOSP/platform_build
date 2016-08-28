3c545c00f5a9a022ffb78ed4300ca04862234c60
Alberto97 <albertop2197@gmail.com>
2016-08-28 21:00:36 +0200

    Fix 29d1a95f4b0b720a005f9a7c7d27f88d993664fa mismerge
    Change-Id: Iec30c2f73d4efdb9ffe15a6488028327068dfb82

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -133,11 +133,11 @@ class EdifyGenerator(object):
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
     cmd = ('assert(' +
-           ' || '.join(['getprop("ro.product.device") == "%s"' % i
-                         for i in device.split(",")]) +
+           ' || '.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
+                         % (i, i) for i in device.split(",")]) +
            ' || abort("E%d: This package is for device: %s; ' +
            'this device is " + getprop("ro.product.device") + ".");' +
-           ');') % (device, common.ErrorCode.DEVICE_MISMATCH)
+           ');') % (common.ErrorCode.DEVICE_MISMATCH, device)
     self.script.append(cmd)
 
   def AssertSomeBootloader(self, *bootloaders):
